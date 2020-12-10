0f0beb35add73e27403ca835c29934e574362342
unknown <bhushankadam2398@gmail.com>
2020-12-10 19:31:57 +0530

    added h1 tag

--- src/app/eighth/eighth.component.ts
+++ src/app/eighth/eighth.component.ts
@@ -9,7 +9,7 @@ export class EighthComponent implements OnInit {
 
   public users = [
     { id: 2, name: 'user 1', email: 'user1@test.com', address: 'pune' },
-    { id: 1, name: 'user 2', email: 'user2@test.com', address: 'mumbai' },
+    { id: 1, name: 'user 2', email: 'user2@test1.com', address: 'mumbai' },
     { id: 3, name: 'user 3', email: 'user3@test.com', address: 'pune' },
     { id: 4, name: 'user 4', email: 'user4@test.com', address: 'karad' },
     { id: 5, name: 'user 5', email: 'user5@test.com', address: 'satara' }
